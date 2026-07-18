694746f614c8fb626dcd5a7ddcc4edcdf4b7bcf2
Elkinx-wevs <elkinpuma2007@gmail.com>
2026-07-17 20:06:34 -0500

    Delete UTNGolCoin.Api directory

--- UTNGolCoin.Api/DTOs/SettlePredictionDto.cs
+++
@@ -1,9 +0,0 @@
-namespace UTNGolCoin.API.DTOs
-{
-    public class SettlePredictionDto
-    {
-        public int MatchId { get; set; }
-        public string OfficialResult { get; set; }
-        public decimal AppliedOdds { get; set; }
-    }
-}
