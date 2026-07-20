3a2f44a08f2cafaca652b205405299dd4ed3da40
Elkinx-wevs <elkinpuma2007@gmail.com>
2026-07-20 17:01:12 -0500

    Delete UTNGolCoin.Api directory

--- UTNGolCoin.Api/DTOs/UserIdRequestDto.cs
+++
@@ -1,7 +0,0 @@
-namespace UTNGolCoin.API.DTOs
-{
-    public class UserIdRequestDto
-    {
-        public int UserId { get; set; }
-    }
-}
--- UTNGolCoin.Api/DTOs/WalletDto.cs
+++
@@ -1,8 +0,0 @@
-namespace UTNGolCoin.API.DTOs
-{
-    public class WalletDto
-    {
-        public int UserId { get; set; }
-        public decimal Balance { get; set; }
-    }
-}
